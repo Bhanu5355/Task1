94ffdaa201d3a2ebd0db9ec931b4a106a94febd1
Bhanu5355 <bhukyabhanu1234@gmail.com>
2025-12-31 10:09:12 +0530

    Initial commit - added JavaScript practice files

---
+++ Arrays.js
@@ -0,0 +1,25 @@
+//Declaring the array
+let bhanu = ["good", 1 , 1.0];
+console.log(bhanu);
+
+
+//Another way
+let m = new Array();
+m[0] = "bhanu"
+m[1] = 1
+console.log(m);
+
+
+//operation in Array
+let items = ["pen", "pencil"]
+items.push("laser") // Add element in the arrays at last
+console.log(items);
+items.pop()//used to remove the element last one
+items.shift()//used to remove the first element
+items.unshift()//used to add the ekement at the first
+
+
+
+
+
+
---
+++ slicesplice.js
@@ -0,0 +1,6 @@
+let n = [10,20,30,40];
+console.log(n.slice(1,3));//removes mentioned indexes 
+
+n.splice(1,2,60,90) // adds 60 and 90 in the indexes
+console.log(n);
+
